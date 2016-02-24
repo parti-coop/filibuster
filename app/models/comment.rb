1d8cacb8e122521140b4a46bd5c5ac07917df0f2
dali <dali@ufofactory.org>
2016-02-24 21:49:30 +0900

    tab comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :opinion
+
+  scope :recent, -> { order(created_at: :desc) }
+  scope :agreed, -> { where(choice: 'agree') }
+  scope :disagreed, -> { where(choice: 'disagree') }
 end
